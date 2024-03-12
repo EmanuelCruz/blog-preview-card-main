d5b6ca97cb1409f715045b1046309eaf34419926
Emanuel Cruz <emanuelcruz091@gmail.com>
2024-03-11 22:40:14 -0300

    config: custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        paragraph: "16px",
+      },
+      fontWeight: {
+        "custom-500": "500",
+        "custom-800": "800",
+      },
+      colors: {
+        "custom-yellow": "hsl(47, 88%, 63%)",
+        "custom-white": "hsl(0, 0%, 100%)",
+        "custom-grey": "hsl(0, 0%, 50%)",
+        "custom-black": "hsl(0, 0%, 7%)",
+      },
+      fontFamily: {
+        main: "figtree",
+      },
+    },
   },
   plugins: ["prettier-plugin-tailwindcss"],
 };
